a955f1c6dec638dff5549476ed6959e69b3509c7
LeeKh4 <108122523+LeeKh4@users.noreply.github.com>
2022-07-29 16:06:30 +0700

    Add files via upload

---
+++ css/text.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typing-1', {
+    strings: ['Ninja', 'Designer', ],
+    smartBackspace: true,
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+    loopCount: Infinity // Default value
+});
